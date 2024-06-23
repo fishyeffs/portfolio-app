59a0f35349f59f1ef24867f26e21ee688c36ce4f
Nick Fisher <nickfish0311@gmail.com>
2024-06-23 13:50:32 +1200

    status message on commit of data

--- server/routers/contactForm.py
+++ server/routers/contactForm.py
@@ -7,7 +7,7 @@ router = APIRouter()
 # author email  : pre-validated front end for user@domain.com
 # msg           : minimum 30 chars message content
 # timestamp     : POSIX timestamp created on button click
+# returns       : status of commit
 @router.get("", tags=[""])
 async def sendEmail(authorEmail: str, msg: str, timestamp: float):
     datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d')
-    return 

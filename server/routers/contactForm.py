f63a61dc06ce878dcf5ceedf3b6db4b208ae8b70
Nick Fisher <nickfish0311@gmail.com>
2024-06-23 14:21:45 +1200

    new git ignore

--- server/routers/contactForm.py
+++ server/routers/contactForm.py
@@ -1,6 +1,9 @@
 from FastAPI import APIRouter
 from mailer import Mailer, Message
 from datetime import datetime
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
 router = APIRouter()
 
@@ -11,3 +14,8 @@ router = APIRouter()
 @router.get("", tags=[""])
 async def sendEmail(authorEmail: str, msg: str, timestamp: float):
     datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d')
+    #initialise creds
+    cred = credentials.ApplicationDefault()
+
+    firebase_admin.initialize_app(cred)
+    db = firestore.client()
